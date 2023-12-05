cf6a92c9749483bc3b8ac49c4731d84930b3a3a3
WU MENGHUNG <wu.menghung@jp.panasonic.com>
2023-12-05 19:38:53 +0900

    fileter ik solution

--- src/hello_ur.cpp
+++ src/hello_ur.cpp
@@ -8,6 +8,27 @@
 
 using moveit::planning_interface::MoveGroupInterface;
 
+void findClosestSolution(
+  const std::vector<double> & current_q,
+  const std::vector<moveit::core::VariableBounds> & joint_bonds,
+  std::vector<double> & solution)
+{
+  for (std::size_t i = 0; i < current_q.size(); ++i) {
+    //RCLCPP_INFO(logger, "Joint %s: %f %f", joint_name[i].c_str(), solution[i], current_q[i]);
+    // find closest angle
+    if (std::fabs(current_q[i] - solution[i]) > M_PI) {
+      if (current_q[i] > solution[i] && solution[i] + 2 * M_PI < joint_bonds[i].max_position_) {
+        solution[i] += 2 * M_PI;
+      } else if (current_q[i] < solution[i] &&
+        solution[i] - 2 * M_PI > joint_bonds[i].min_position_)
+      {
+        solution[i] -= 2 * M_PI;
+      }
+    }
+    //RCLCPP_INFO(logger, "Joint %s: %f", joint_name[i].c_str(), solution[i]);
+  }
+}
+
 bool planAndExecutePose(
   const geometry_msgs::msg::Pose & target_pose,
   const std::function<void(std::string)> draw_title,
@@ -63,6 +84,7 @@ int main(int argc, char * argv[])
   // Create the MoveIt MoveGroup Interface
   auto move_group_interface = MoveGroupInterface(node, arm_group);
 
+
   // Construct and initialize MoveItVisualTools
   auto moveit_visual_tools =
     moveit_visual_tools::MoveItVisualTools{node, base_link,
@@ -70,6 +92,9 @@ int main(int argc, char * argv[])
     move_group_interface.getRobotModel()};
   moveit_visual_tools.deleteAllMarkers();
   moveit_visual_tools.loadRemoteControl();
+
+  //auto robot_model = move_group_interface.getRobotModel();
+
 
   // set velocity
   move_group_interface.setMaxVelocityScalingFactor(1.0);
@@ -132,49 +157,167 @@ int main(int argc, char * argv[])
   // Add the collision object to the scene
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
   planning_scene_interface.applyCollisionObject(collision_object);
+
   ////////////////////////////////////////////
   moveit::core::RobotStatePtr current_state = move_group_interface.getCurrentState(10);
   const moveit::core::JointModelGroup * joint_model_group =
     move_group_interface.getCurrentState()->getJointModelGroup(arm_group);
+
+  // RCLCPP_INFO(logger, "Available Planning Groups:");
+  // std::copy(
+  //   move_group_interface.getJointModelGroupNames().begin(),
+  //   move_group_interface.getJointModelGroupNames().end(),
+  //   std::ostream_iterator<std::string>(std::cout, ", "));
+
   // Next get the current set of joint values for the group.
-  std::vector<double> joint_group_positions;
-  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
-  for (double q : joint_group_positions) {
-    RCLCPP_INFO_STREAM(logger, "q: " << q);
+  std::vector<double> seed_state;
+  current_state->copyJointGroupPositions(joint_model_group, seed_state);
+  for (double q : seed_state) {
+    RCLCPP_INFO_STREAM(logger, "seed_state q: " << q);
   }
   auto joint_name = joint_model_group->getActiveJointModelNames();
+  std::vector<moveit::core::VariableBounds> joint_bonds;
   for (std::string name : joint_name) {
     RCLCPP_INFO_STREAM(logger, "jnt: " << name);
+    auto joint_model = joint_model_group->getJointModel(name);
+    // joint_model->getType(); // prismatic, revolute
+    auto bonds = joint_model->getVariableBounds(name);
+    RCLCPP_INFO_STREAM(logger, "max=" << bonds.max_position_ << ", min=" << bonds.min_position_);
+    joint_bonds.push_back(bonds);
   }
   RCLCPP_INFO_STREAM(logger, "planning time: " << move_group_interface.getPlanningTime());
+
+  // ////////////setFromIK/////////////////////
+  // robot_model_loader::RobotModelLoader robot_model_loader(node);
+  // const moveit::core::RobotModelPtr & kinematic_model = robot_model_loader.getModel();
+  // //moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
+  // current_state->setToDefaultValues();
+  // double timeout = 0.1;
+  // const Eigen::Isometry3d & end_effector_state = current_state->getGlobalLinkTransform(
+  //   "wrist_3_link");
+  // bool found_ik = current_state->setFromIK(joint_model_group, end_effector_state, timeout);
+
+  // // Now, we can print out the IK solution (if found):
+  // if (found_ik) {
+  //   current_state->copyJointGroupPositions(joint_model_group, seed_state);
+  //   for (std::size_t i = 0; i < seed_state.size(); ++i) {
+  //     RCLCPP_INFO(logger, "Joint %s: %f", joint_name[i].c_str(), seed_state[i]);
+  //   }
+  // } else {
+  //   RCLCPP_INFO(logger, "Did not find IK solution");
+  // }
+
   //move_group_interface.allowReplanning(true);
   //move_group_interface.setReplanAttempts(5);
 
-  [&move_group_interface, &joint_group_positions]() {
-    joint_group_positions[0] = 0.0; // shoulder_pan_joint
-    joint_group_positions[1] = -2.689; //  shoulder_lift_joint
-    joint_group_positions[2] = 0.7441; // elbow_joint
-    joint_group_positions[3] = -2.768; //  wrist_1_joint
-    joint_group_positions[4] = 1.57; //  wrist_2_joint
-    joint_group_positions[5] = 1.57; // wrist_3_joint
-    move_group_interface.setJointValueTarget(joint_group_positions);
-    moveit::planning_interface::MoveGroupInterface::Plan plan;
-    if (move_group_interface.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS) {
-      move_group_interface.execute(plan);
-    }
-  } ();
+  // [&move_group_interface, &seed_state]() {
+  //   seed_state[0] = 0.0; // shoulder_pan_joint
+  //   seed_state[1] = -2.689; //  shoulder_lift_joint
+  //   seed_state[2] = 0.7441; // elbow_joint
+  //   seed_state[3] = -2.768; //  wrist_1_joint
+  //   seed_state[4] = 1.57; //  wrist_2_joint
+  //   seed_state[5] = 1.57; // wrist_3_joint
+  //   move_group_interface.setJointValueTarget(seed_state);
+  //   moveit::planning_interface::MoveGroupInterface::Plan plan;
+  //   if (move_group_interface.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS) {
+  //     move_group_interface.execute(plan);
+  //   }
+  // } ();
 
-  [&move_group_interface, &joint_group_positions, &logger]() {
-    joint_group_positions[0] = 0.13249; // shoulder_pan_joint
-    joint_group_positions[1] = -1.8636;  //  shoulder_lift_joint
-    joint_group_positions[2] = -1.8725;  // elbow_joint
-    joint_group_positions[3] = -0.977; //  wrist_1_joint
-    joint_group_positions[4] = 1.57; //  wrist_2_joint
-    joint_group_positions[5] = 1.703; // wrist_3_joint
-    move_group_interface.setJointValueTarget(joint_group_positions);
+  // [&move_group_interface, &seed_state, &logger]() {
+  //   seed_state[0] = 0.13249; // shoulder_pan_joint
+  //   seed_state[1] = -1.8636;  //  shoulder_lift_joint
+  //   seed_state[2] = -1.8725;  // elbow_joint
+  //   seed_state[3] = -0.977; //  wrist_1_joint
+  //   seed_state[4] = 1.57; //  wrist_2_joint
+  //   seed_state[5] = 1.703; // wrist_3_joint
+  //   move_group_interface.setJointValueTarget(seed_state);
+  //   moveit::planning_interface::MoveGroupInterface::Plan plan;
+  //   [&]()->void {
+  //     for (int i = 3; i > 0; i--) {
+  //       if (move_group_interface.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS) {
+  //         move_group_interface.execute(plan);
+  //         return;
+  //       }
+  //       RCLCPP_WARN_STREAM(logger, "try to replan");
+  //     }
+  //     RCLCPP_ERROR_STREAM(logger, "planning fail");
+  //   }();
+  // } ();
+
+
+  // Set a target Pose
+  auto const target_pose = [] {
+      geometry_msgs::msg::Pose msg;
+      msg.orientation.w = 0.0;
+      msg.orientation.x = 1.0;
+      msg.orientation.y = 0.0;
+      msg.orientation.z = 0.0;
+      msg.position.x = -0.625;
+      msg.position.y = 0.133;
+      msg.position.z = 0.614;
+      return msg;
+    }();
+
+  auto ik_solver = joint_model_group->getSolverInstance();
+  std::vector<double> solution;
+  moveit_msgs::msg::MoveItErrorCodes err_code;
+  RCLCPP_INFO_STREAM(logger, "timeout " << joint_model_group->getDefaultIKTimeout());
+  // joint_model_group->printGroupInfo(std::cout);
+  RCLCPP_INFO_STREAM(logger, "timeout " << ik_solver->getDefaultTimeout());
+  std::vector<double> current_q;
+  current_state->copyJointGroupPositions(joint_model_group, current_q);
+
+  current_state->setToDefaultValues();
+  current_state->copyJointGroupPositions(joint_model_group, seed_state);
+  ik_solver->getPositionIK(target_pose, seed_state, solution, err_code);
+  //ik_solver->searchPositionIK(target_pose, seed_state, 0.05, solution, err_code);
+  RCLCPP_INFO_STREAM(logger, "errcode: " << err_code.val);
+  findClosestSolution(current_q, joint_bonds, solution);
+
+  move_group_interface.setJointValueTarget(solution);
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+  if (move_group_interface.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS) {
+    move_group_interface.execute(plan);
+  }
+
+  // if (!planAndExecutePose(
+  //     target_pose, draw_title, prompt, draw_trajectory_tool_path,
+  //     move_group_interface))
+  // {
+  //   RCLCPP_ERROR(logger, "Planning failed!");
+  // }
+
+  auto const target_pose2 = [] {
+      geometry_msgs::msg::Pose msg;
+      msg.orientation.w = 0.0;
+      msg.orientation.x = 1.0;
+      msg.orientation.y = 0.0;
+      msg.orientation.z = 0.0;
+      msg.position.x = -0.56;
+      msg.position.y = 0.06;
+      msg.position.z = 0.25;
+
+      return msg;
+    }();
+
+  current_state = move_group_interface.getCurrentState(10);
+  current_state->copyJointGroupPositions(joint_model_group, seed_state);
+
+  ik_solver->getPositionIK(target_pose2, seed_state, solution, err_code);
+  //ik_solver->searchPositionIK(target_pose, seed_state, 0.05, solution, err_code);
+  RCLCPP_INFO_STREAM(logger, "errcode: " << err_code.val);
+
+  findClosestSolution(seed_state, joint_bonds, solution);
+  for (std::size_t i = 0; i < solution.size(); ++i) {
+    RCLCPP_INFO(logger, "solu %s: %f", joint_name[i].c_str(), solution[i]);
+  }
+
+  [&move_group_interface, &solution, &logger]() {
+    move_group_interface.setJointValueTarget(solution);
     moveit::planning_interface::MoveGroupInterface::Plan plan;
     [&]()->void {
-      for (int i = 3; i > 0; i--) {
+      for (int i = 30; i > 0; i--) {
         if (move_group_interface.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS) {
           move_group_interface.execute(plan);
           return;
@@ -185,39 +328,6 @@ int main(int argc, char * argv[])
     }();
   } ();
 
-
-  // // Set a target Pose
-  // auto const target_pose = [] {
-  //     geometry_msgs::msg::Pose msg;
-  //     msg.orientation.w = 0.0;
-  //     msg.orientation.x = 1.0;
-  //     msg.orientation.y = 0.0;
-  //     msg.orientation.z = 0.0;
-  //     msg.position.x = -0.625;
-  //     msg.position.y = 0.133;
-  //     msg.position.z = 0.614;
-  //     return msg;
-  //   }();
-
-  // if (!planAndExecutePose(
-  //     target_pose, draw_title, prompt, draw_trajectory_tool_path,
-  //     move_group_interface))
-  // {
-  //   RCLCPP_ERROR(logger, "Planning failed!");
-  // }
-
-  // auto const target_pose2 = [] {
-  //     geometry_msgs::msg::Pose msg;
-  //     msg.orientation.w = 0.0;
-  //     msg.orientation.x = 1.0;
-  //     msg.orientation.y = 0.0;
-  //     msg.orientation.z = 0.0;
-  //     msg.position.x = -0.56;
-  //     msg.position.y = 0.06;
-  //     msg.position.z = 0.25;
-
-  //     return msg;
-  //   }();
 
   // if (!planAndExecutePose(
   //     target_pose2, draw_title, prompt, draw_trajectory_tool_path,
